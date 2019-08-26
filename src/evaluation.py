d73ae2bef01377f9bacd556bc3c38c4795e7ff97
Ivona <najdenkoskai@gmail.com>
2019-08-26 19:17:02 +0200

    Added precision, recall score methods. gitignore updated.

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,5 +1,7 @@
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 
 def get_model_accuracy(true_y, pred_y):
@@ -8,6 +10,14 @@ def get_model_accuracy(true_y, pred_y):
 
 def get_model_confusion_matrix(true_y, pred_y):
     return confusion_matrix(true_y, pred_y).ravel()
+
+
+def get_precision_score(true_y, pred_y):
+    return precision_score(true_y, pred_y)
+
+
+def get_recall_score(true_y, pred_y):
+    return recall_score(true_y, pred_y)
 
 
 def calculate_ratios(tp, fp, fn, tn):
